88540483eb53e64a42335db2f5994193ff024e53
Wale <onafesowale@gmail.com>
2024-02-23 08:52:09 +0100

    delete brand

---
+++ src/IMS.Application/Features/Brand/Commands/DeleteBrand/DeleteBrandCommand.cs
@@ -0,0 +1,5 @@
+﻿using IMS.Domain.Shared;
+using MediatR;
+
+namespace IMS.Application.Features.Brand.Commands.DeleteBrand;
+public record DeleteBrandCommand(Guid BrandId) : IRequest<Result<Unit, Error>>;
---
+++ src/IMS.Application/Features/Brand/Commands/DeleteBrand/DeleteBrandCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using IMS.Application.Shared;
+using IMS.Domain.Primitives;
+using IMS.Domain.Shared;
+using IMS.Infrastructure.Data.Specifications.BrandSpecifications;
+using MediatR;
+
+namespace IMS.Application.Features.Brand.Commands.DeleteBrand;
+internal sealed class DeleteBrandCommandHandler : IRequestHandler<DeleteBrandCommand, Result<Unit, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public DeleteBrandCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Unit, Error>> Handle(DeleteBrandCommand request, CancellationToken cancellationToken)
+    {
+        var spec = new GetBrandSpecification(request.BrandId);
+
+        var brand = await _unitOfWork.Repository<BrandEntity>().GetAsync(spec);
+
+        if (brand is null)
+            return ApplicationErrors.Brand.NOTFOUND(request.BrandId);
+
+        var res = brand.Delete();
+
+        if (res.IsFailure)
+            return ApplicationErrors.Brand.BADREQUEST("An error occurred!!");
+
+        _unitOfWork.Repository<BrandEntity>().Update(res.Value);
+
+        await _unitOfWork.Complete();
+
+        return Unit.Value;
+    }
+}
