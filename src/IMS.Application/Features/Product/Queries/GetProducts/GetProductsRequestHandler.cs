b3d3cb2858c60b7bd7ac3acd66377f0fcf01acc4
Wale <onafesowale@gmail.com>
2024-02-23 08:58:48 +0100

    get products handler

---
+++ src/IMS.Application/Features/Product/Queries/GetProducts/GetProductsRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using IMS.Application.Features.Product.Queries.GetProduct;
+using IMS.Domain.Primitives;
+using IMS.Domain.Shared;
+using MediatR;
+
+namespace IMS.Application.Features.Product.Queries.GetProducts;
+internal sealed class GetProductsRequestHandler : IRequestHandler<GetProductsRequest,
+    Result<IReadOnlyList<GetProductResponse>, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public GetProductsRequestHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<IReadOnlyList<GetProductResponse>, Error>> Handle(GetProductsRequest request, 
+        CancellationToken cancellationToken)
+    {
+        var products = await _unitOfWork.Repository<ProductEntity>()
+            .GetAllAsync(cancellationToken);
+
+        var res = _mapper.Map<IReadOnlyList<GetProductResponse>>(products);
+
+        return new(res);
+    }
+}
