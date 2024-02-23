b042406b6bfe2b88dbbfdbaf7170543ef4b9e588
Wale <onafesowale@gmail.com>
2024-02-23 08:58:01 +0100

    brand response

---
+++ src/IMS.Application/Features/Brand/Queries/GetBrandResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace IMS.Application.Features.Brand.Queries;
+public class GetBrandResponse
+{
+    public Guid BrandId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; } 
+}
