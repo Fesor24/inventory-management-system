02f1f51c31fc16a2f4b0d26960cf43b2570ba2be
Wale <onafesowale@gmail.com>
2024-02-23 08:56:19 +0100

    base auditable entity

---
+++ src/IMS.Domain/Primitives/BaseAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace IMS.Domain.Primitives;
+public abstract class BaseAuditableEntity<TKey> : Entity<TKey> where TKey: IEquatable<TKey>
+{
+    public BaseAuditableEntity(TKey key): base(key) { }
+    public BaseAuditableEntity()
+    {
+        
+    }
+
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set;}
+}
