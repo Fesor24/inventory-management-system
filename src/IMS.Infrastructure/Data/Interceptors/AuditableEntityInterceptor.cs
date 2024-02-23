9c76caba9e6b2228654e0d0d98f9ce2c0b30a649
Wale <onafesowale@gmail.com>
2024-02-23 08:57:43 +0100

    interceptor

---
+++ src/IMS.Infrastructure/Data/Interceptors/AuditableEntityInterceptor.cs
@@ -0,0 +1,38 @@
+﻿using IMS.Domain.Primitives;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace IMS.Infrastructure.Data.Interceptors;
+internal class AuditableEntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(
+        DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateAuditableEntities(eventData.Context);
+
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(
+        DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateAuditableEntities(eventData.Context);
+
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    internal static void UpdateAuditableEntities(DbContext? context)
+    {
+        if (context is null) return;
+
+        var entities = context.ChangeTracker.Entries<BaseAuditableEntity<Guid>>().ToList();
+
+        foreach(var entity in entities)
+        {
+            entity.Entity.UpdatedAt = DateTime.UtcNow;
+
+            if(entity.State == EntityState.Added)
+                entity.Entity.CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
