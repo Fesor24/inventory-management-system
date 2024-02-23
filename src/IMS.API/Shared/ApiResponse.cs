67a8901d320195ffbcadc4050053bfd32fbc94fa
Wale <onafesowale@gmail.com>
2024-02-23 08:53:24 +0100

    api response

---
+++ src/IMS.API/Shared/ApiResponse.cs
@@ -0,0 +1,31 @@
+﻿using IMS.Domain.Shared;
+
+namespace IMS.API.Shared;
+
+public class ApiResponse
+{
+    public bool IsSuccess { get; private set; }
+
+    public Error Error { get; private set; }
+
+    public ApiResponse()
+    {
+        IsSuccess = true;
+    }
+
+    public ApiResponse(Error error)
+    {
+        IsSuccess = false;
+        Error = error;
+    }
+}
+
+public class ApiResponse<TData> : ApiResponse
+{
+    public ApiResponse(TData data) : base()
+    {
+        Data = data;
+    }
+
+    public TData Data { get; private set; }
+}
