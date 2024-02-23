6279d68c4cbc2ef9913022130700b831ba10bf9c
Wale <onafesowale@gmail.com>
2024-02-23 08:51:18 +0100

    endpoint extensions

---
+++ src/IMS.API/Extensions/EndpointExtensions.cs
@@ -0,0 +1,20 @@
+﻿using IMS.API.Abstractions;
+using System.Reflection;
+
+namespace IMS.API.Extensions;
+
+public static class EndpointExtensions
+{
+    public static void RegisterEndpoints(this WebApplication app)
+    {
+        var endpoints = Assembly.GetExecutingAssembly()
+            .GetExportedTypes()
+            .Where(x => x.IsAssignableTo(typeof(IEndpointRegistration)) && !x.IsAbstract && !x.IsInterface)
+            .Select(Activator.CreateInstance)
+            .Cast<IEndpointRegistration>()
+            .ToList();
+
+        foreach (var endpoint in endpoints)
+            endpoint.RegisterEndpoint(app);
+    }
+}
