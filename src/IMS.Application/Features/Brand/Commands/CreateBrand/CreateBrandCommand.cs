5c246473ed87413cdbeedd5d4beed8af169dd42b
Wale <onafesowale@gmail.com>
2024-02-23 08:51:58 +0100

    create brand

---
+++ src/IMS.Application/Features/Brand/Commands/CreateBrand/CreateBrandCommand.cs
@@ -0,0 +1,5 @@
+﻿using IMS.Domain.Shared;
+using MediatR;
+
+namespace IMS.Application.Features.Brand.Commands.CreateBrand;
+public record CreateBrandCommand(string Name, string Description) : IRequest<Result<Guid, Error>>;
---
+++ src/IMS.Application/Features/Brand/Commands/CreateBrand/CreateBrandCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using IMS.Application.Shared;
+using IMS.Domain.Primitives;
+using IMS.Domain.Shared;
+using MediatR;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleToAttribute("IMS.UnitTests")]
+
+namespace IMS.Application.Features.Brand.Commands.CreateBrand;
+internal sealed class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand,
+    Result<Guid, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateBrandCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Guid, Error>> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+    {
+        var res = BrandEntity.Create(request.Name, request.Description);
+
+        if (res.IsFailure)
+            return ApplicationErrors.Brand.BADREQUEST(res.Error.Message);
+
+        await _unitOfWork.Repository<BrandEntity>()
+            .AddAsync(res.Value, cancellationToken);
+
+        await _unitOfWork.Complete();
+
+        return res.Value.Id;
+    }
+}
