4f20e42ff3a2e7328470ddd55f286f5dd57d43f9
Wale <onafesowale@gmail.com>
2024-02-23 08:56:04 +0100

    product aggregates

---
+++ src/IMS.Domain/Entities/ProductAggregates/ProductInfo.cs
@@ -0,0 +1,2 @@
+﻿namespace IMS.Domain.Entities.ProductAggregates;
+public record ProductInfo(string Name, string Description);
---
+++ src/IMS.Domain/Entities/ProductAggregates/ProductStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Serialization;
+
+namespace IMS.Domain.Entities.ProductAggregates;
+public enum ProductStatus
+{
+    [EnumMember(Value = "Unknown")]
+    Unknown,
+    [EnumMember(Value = "SecondHand")]
+    SecondHand,
+    [EnumMember(Value = "Refurbished")]
+    Refurbished,
+    [EnumMember(Value = "New")]
+    New
+}
