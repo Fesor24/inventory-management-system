a0f9db7e7ee15cbc8e8725d050257bae4f7716dd
Wale <onafesowale@gmail.com>
2024-02-23 08:56:42 +0100

    DI

---
+++ src/IMS.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using IMS.Domain.Primitives;
+using IMS.Infrastructure.Data;
+using IMS.Infrastructure.Data.Interceptors;
+using IMS.Infrastructure.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace IMS.Infrastructure;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddSingleton<AuditableEntityInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, opt) =>
+        {
+            opt.UseNpgsql(config.GetConnectionString(ApplicationDbContext.DATABASE_CONNECTION),
+                mig => mig.MigrationsAssembly(Assembly.GetExecutingAssembly().FullName))
+            .AddInterceptors(sp.GetRequiredService<AuditableEntityInterceptor>()
+            );
+        });
+
+        //services.AddDbContext<ApplicationDbContext>((sp, opt) =>
+        //{
+        //    opt.UseInMemoryDatabase("IMS")
+        //    .AddInterceptors(sp.GetRequiredService<AuditableEntityInterceptor>());
+        //});
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
