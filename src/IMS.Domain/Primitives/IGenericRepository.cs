882e7232f8fa89054720ae3e54a817bd05b01dcf
Wale <onafesowale@gmail.com>
2024-02-23 08:56:37 +0100

    repository

---
+++ src/IMS.Domain/Primitives/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace IMS.Domain.Primitives;
+public interface IGenericRepository<TEntity> 
+    where TEntity: class, new()
+{
+    Task<TEntity?> GetAsync(ISpecification<TEntity> spec, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TEntity>> GetAllAsync(ISpecification<TEntity> spec, CancellationToken cancellationToken = default);
+    Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task AddAsync(TEntity entity, CancellationToken cancellationToken = default);
+    Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+}
---
+++ src/IMS.Infrastructure/Data/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using IMS.Domain.Primitives;
+using IMS.Infrastructure.Data.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Infrastructure.Data.Repositories;
+internal class GenericRepository<TEntity> : IGenericRepository<TEntity>
+    where TEntity : class, new()
+{
+    private readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
+    {
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task AddAsync(TEntity entity, CancellationToken cancellationToken = default) =>
+        await _dbSet.AddAsync(entity, cancellationToken);
+
+    public async Task AddRangeAsync(IEnumerable<TEntity> entities, CancellationToken cancellationToken = default) =>
+        await _dbSet.AddRangeAsync(entities, cancellationToken);
+
+    public void Delete(TEntity entity) =>
+        _dbSet.Remove(entity);
+
+    public async Task<IReadOnlyList<TEntity>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        await _dbSet.ToListAsync(cancellationToken);
+
+    public async Task<IReadOnlyList<TEntity>> GetAllAsync(ISpecification<TEntity> spec, 
+        CancellationToken cancellationToken = default) =>
+        await ApplySpecification(spec).ToListAsync(cancellationToken);
+
+    public async Task<TEntity?> GetAsync(ISpecification<TEntity> spec, 
+        CancellationToken cancellationToken = default) =>
+        await ApplySpecification(spec).FirstOrDefaultAsync(cancellationToken);
+
+    public void Update(TEntity entity)
+    {
+        _dbSet.Attach(entity);
+        _dbSet.Entry(entity).State = EntityState.Modified;
+    }
+
+    private IQueryable<TEntity> ApplySpecification(ISpecification<TEntity> spec) =>
+        SpecificationEvaluator.GetQuery(_dbSet.AsQueryable(), spec);
+}
