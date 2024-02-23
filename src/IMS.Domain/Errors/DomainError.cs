c7fe6e9a7ec79d12019ad18b0f365ea41eaef1d4
Wale <onafesowale@gmail.com>
2024-02-23 08:57:03 +0100

    errors

---
+++ src/IMS.Domain/Errors/DomainError.cs
@@ -0,0 +1,36 @@
+﻿using IMS.Domain.Shared;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleToAttribute("IMS.UnitTests")]
+namespace IMS.Domain.Errors;
+internal static class DomainError
+{
+    internal static class Brand
+    {
+        internal static Error MaximumLengthExceeded(string property, int length) => 
+            new("", $"'{property}' property exceeds maximum character length: {length}");
+
+        internal static Error InvalidName = new("", "Specify a valid brand name");
+    }
+
+    internal static class Product
+    {
+        internal static Error MaximumLengthExceeded(string property, int length) =>
+            new("", $"'{property}' property exceeds maximum character length: {length}");
+
+        internal static Error InvalidQuantity = new("", "Product quantity can not be less than 0");
+
+        internal static Error InvalidPrice = new("", "Product price can not be less than 0");
+
+        internal static Error InvalidName = new("", "Specify a valid product name");
+        internal static Error InvalidStatus = new("", "Select a valid status enum. Unknown=0;SecondHand=1;Refurbished=2;New=3");
+    }
+
+    internal static class Category
+    {
+        internal static Error MaximumLengthExceeded(string property, int length) =>
+            new("", $"'{property}' property exceeds maximum character length: {length}");
+
+        internal static Error InvalidName = new("", "Specify a valid category name");
+    }
+}
---
+++ src/IMS.Domain/Shared/Error.cs
@@ -0,0 +1,14 @@
+﻿namespace IMS.Domain.Shared;
+public class Error
+{
+    public static Error None = new(string.Empty, string.Empty);
+
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; private set; }
+    public string Message { get; private set; }
+}
