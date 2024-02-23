005f084787b144cf0d4f694d3b1c424d154a5ae9
Wale <onafesowale@gmail.com>
2024-02-23 08:58:08 +0100

    unit of work

---
+++ src/IMS.Domain/Primitives/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace IMS.Domain.Primitives;
+public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<TEntity> Repository<TEntity>()
+          where TEntity : class, new();
+
+    Task<int> Complete();
+}
---
+++ src/IMS.Infrastructure/Data/Repositories/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using IMS.Domain.Primitives;
+using System.Collections;
+
+namespace IMS.Infrastructure.Data.Repositories;
+internal class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private Hashtable _repo;
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<int> Complete() =>
+        await _context.SaveChangesAsync();
+
+    public void Dispose() => 
+        _context.Dispose();
+
+    public IGenericRepository<TEntity> Repository<TEntity>()
+        where TEntity : class, new()
+    {
+        _repo ??= new();
+
+        string key = typeof(TEntity).Name;
+
+        if (!_repo.ContainsKey(key))
+        {
+            var genericType = typeof(GenericRepository<>);
+
+            var genericInstance = Activator.CreateInstance(genericType.MakeGenericType(typeof(TEntity)), _context);
+
+            _repo.Add(key, genericInstance);
+        }
+
+        return (IGenericRepository<TEntity>)_repo[key];
+
+        
+    }
+}
