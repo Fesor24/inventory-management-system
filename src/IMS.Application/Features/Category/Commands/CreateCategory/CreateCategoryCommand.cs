cc76a84389e15c21e273d1382223edf150e0d1b7
Wale <onafesowale@gmail.com>
2024-02-23 08:52:33 +0100

    create category

---
+++ src/IMS.Application/Features/Category/Commands/CreateCategory/CreateCategoryCommand.cs
@@ -0,0 +1,6 @@
+﻿using IMS.Domain.Shared;
+using MediatR;
+
+namespace IMS.Application.Features.Category.Commands.CreateCategory;
+public record CreateCategoryCommand(string Name, string Description, Guid BrandId) :
+    IRequest<Result<Guid, Error>>;
---
+++ src/IMS.Application/Features/Category/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using IMS.Application.Shared;
+using IMS.Domain.Primitives;
+using IMS.Domain.Shared;
+using MediatR;
+
+namespace IMS.Application.Features.Category.Commands.CreateCategory;
+internal sealed class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand,
+    Result<Guid, Error>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateCategoryCommandHandler(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<Result<Guid, Error>> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var res = CategoryEntity.Create(request.Name, request.Description, request.BrandId);
+
+        if (res.IsFailure)
+            return ApplicationErrors.Brand.BADREQUEST(res.Error.Message);
+
+        await _unitOfWork.Repository<CategoryEntity>()
+            .AddAsync(res.Value, cancellationToken);
+
+        await _unitOfWork.Complete();
+
+        return res.Value.Id;
+    }
+}
