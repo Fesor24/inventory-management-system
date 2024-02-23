75fa9efbfab2fa7bbc0c1bbeee9b79989485b84b
Wale <onafesowale@gmail.com>
2024-02-23 08:58:27 +0100

    ispecification

---
+++ src/IMS.Domain/Primitives/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace IMS.Domain.Primitives;
+public interface ISpecification<TEntity> 
+    where TEntity: class, new()
+{
+    Expression<Func<TEntity, bool>> Criteria { get; }
+    Expression<Func<TEntity, object>> OrderByDescending { get; }
+    List<Expression<Func<TEntity, object>>> Includes { get; }
+}
