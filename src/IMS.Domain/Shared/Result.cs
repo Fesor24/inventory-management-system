021eae30764c22c3d89646e3ac3b26b1b5f49c5d
Wale <onafesowale@gmail.com>
2024-02-23 08:56:51 +0100

    result obj

---
+++ src/IMS.Domain/Shared/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.Shared;
+public class Result
+{
+    protected Result()
+    {
+        IsSuccess = true;
+        Error = Error.None;
+    }
+
+    protected Result(Error error)
+    {
+        IsSuccess = false;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; private set; }
+    public bool IsFailure => !IsSuccess;
+
+    public Error Error { get; private set; }
+
+    public static Result Success => new();
+
+    public static Result Failure(Error error) => new(error);
+}
---
+++ src/IMS.Domain/Shared/ResultT.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.Domain.Shared;
+public class Result<TValue, TError> : Result where TError : Error
+{
+    private readonly TValue _value;
+    private readonly TError _error;
+
+    public Result(TValue value) : base()
+    {
+        _value = value;
+    }
+
+    public Result(TError error) : base(error)
+    {
+        _error = error;
+    }
+
+    public TValue Value => IsSuccess ? _value :
+        throw new Exception("Value can not be accessed");
+
+    public static implicit operator Result<TValue, TError>(TValue value) => new(value);
+
+    public static implicit operator Result<TValue, TError>(TError error) => new(error);
+
+    public TResult Match<TResult>(Func<TValue, TResult> success, Func<TError, TResult> failure) =>
+        IsSuccess ? success(_value) : failure(_error);
+}
